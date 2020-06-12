ef4adf672f826c4938eb5de000ff6c11c8e67b93
Riccardo Rizzo <r.rizzo@arduino.cc>
2020-06-12 10:22:53 +0200

    added ecxample that show how to sue the servo pins

---
+++ examples/ServoPinManagement/ServoPinManagement.ino
@@ -0,0 +1,65 @@
+/*
+  This example shows how to change the configuration mode 
+  of the servo pin on MKR Motor Carrier
+  to use it:
+  - short the servo1 and servo2 data pins
+  - power the motorcarrier trhough the gnd and V connector on the shield
+  - open the setial montior and see the satus change each 500 ms
+ */
+
+
+#include <Arduino_PMIC.h>
+#include <MKRMotorCarrier.h>
+//#include <MKRMotorCarrier_REV2.h>
+#define INTERRUPT_PIN 6
+
+
+void setup()
+{
+  //Serial port initialization
+  Serial.begin(115200);
+  while (!Serial);
+
+  //Establishing the communication with the motor shield
+  if (controller.begin())
+  {
+    Serial.print("MKR Motor Shield connected, firmware version ");
+    Serial.println(controller.getFWVersion());
+  }
+  else
+  {
+    Serial.println("Couldn't connect! Is the red led blinking? You may need to update the firmware with FWUpdater sketch");
+    while (1);
+  }
+
+  // Reboot the motor controller; brings every value back to default
+  Serial.println("reboot");
+  controller.reboot();
+  delay(500);
+
+  PMIC.begin();
+  if (!PMIC.enableBoostMode()) {
+    Serial.println("Error enabling Boost Mode");
+  }
+
+     // to use as input change OUTPUT with INPUT
+      servo1.setPinMode(INPUT);
+    servo3.setPinMode(OUTPUT);
+}
+
+int status = HIGH;
+void loop() {
+    // you can use: servo1, servo2, servo3 or servo4to use the properly servo you want
+    servo3.writeOutput(HIGH);
+
+    Serial.println(servo1.readInput());
+    delay(1000);
+    servo3.writeOutput(LOW);
+
+    Serial.println(servo1.readInput());
+
+
+  controller.ping();
+  //wait
+  delay(1000);
+}
